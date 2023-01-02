85fd9b98bdce9f13fd427ec30dc0c3e2d014dc78
Samuel <pc@Aoba>
2023-01-02 14:35:23 -0300

    consertando o errado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let sliderElement = document.querySelector ("#slider")
 let buttonElement = document.querySelector ("#button")
 
 let sizePassword = document.querySelector ("#valor")
-let password = document.querySelector ("#passord")
+let password = document.querySelector ("#password")
 
 let containerPassword = document.querySelector ("#container-password")
 
